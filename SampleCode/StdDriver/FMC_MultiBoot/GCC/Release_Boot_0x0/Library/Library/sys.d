80f1cb7e84cbb2e4105f6e8e6130ed8077fa7b3e
kchuang1 <kchuang1@nuvoton.com>
2024-12-13 14:39:52 +0800

    1st BSP for CM2003
    Change-Id: Ic8969ae1bb6b50ef7e862771bd7cdfa2a079cfda

---
+++ SampleCode/StdDriver/FMC_MultiBoot/GCC/Release_Boot_0x0/Library/Library/sys.d
@@ -0,0 +1,78 @@
+Library/Library/sys.o: \
+ E:/Working_zone/M23/5_CM2003/1_bsp_20241211_2/Library/StdDriver/src/sys.c \
+ e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\device\nuvoton\cm2003\include\numicro.h \
+ e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\device\nuvoton\cm2003\include\CM2003.h \
+ e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\cmsis\core\include\core_cm23.h \
+ e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\cmsis\core\include\cmsis_version.h \
+ e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\cmsis\core\include\cmsis_compiler.h \
+ e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\cmsis\core\include\cmsis_gcc.h \
+ e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\cmsis\core\include\m-profile/armv8m_mpu.h \
+ e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\device\nuvoton\cm2003\include\system_CM2003.h \
+ e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\device\nuvoton\cm2003\include\adc_reg.h \
+ e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\device\nuvoton\cm2003\include\clk_reg.h \
+ e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\device\nuvoton\cm2003\include\ecap_reg.h \
+ e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\device\nuvoton\cm2003\include\fmc_reg.h \
+ e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\device\nuvoton\cm2003\include\gpio_reg.h \
+ e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\device\nuvoton\cm2003\include\i2c_reg.h \
+ e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\device\nuvoton\cm2003\include\pwm_reg.h \
+ e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\device\nuvoton\cm2003\include\sys_reg.h \
+ e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\device\nuvoton\cm2003\include\timer_reg.h \
+ e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\device\nuvoton\cm2003\include\uart_reg.h \
+ e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\device\nuvoton\cm2003\include\uuart_reg.h \
+ e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\device\nuvoton\cm2003\include\ui2c_reg.h \
+ e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\device\nuvoton\cm2003\include\uspi_reg.h \
+ e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\device\nuvoton\cm2003\include\wdt_reg.h \
+ e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\device\nuvoton\cm2003\include\wwdt_reg.h \
+ e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\stddriver\inc\adc.h \
+ e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\stddriver\inc\clk.h \
+ e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\stddriver\inc\ecap.h \
+ e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\stddriver\inc\fmc.h \
+ e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\stddriver\inc\gpio.h \
+ e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\stddriver\inc\i2c.h \
+ e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\stddriver\inc\sys.h \
+ e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\stddriver\inc\timer.h \
+ e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\stddriver\inc\uart.h \
+ e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\stddriver\inc\pwm.h \
+ e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\stddriver\inc\usci_i2c.h \
+ e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\stddriver\inc\usci_spi.h \
+ e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\stddriver\inc\usci_uart.h \
+ e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\stddriver\inc\wdt.h \
+ e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\stddriver\inc\wwdt.h
+e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\device\nuvoton\cm2003\include\numicro.h:
+e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\device\nuvoton\cm2003\include\CM2003.h:
+e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\cmsis\core\include\core_cm23.h:
+e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\cmsis\core\include\cmsis_version.h:
+e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\cmsis\core\include\cmsis_compiler.h:
+e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\cmsis\core\include\cmsis_gcc.h:
+e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\cmsis\core\include\m-profile/armv8m_mpu.h:
+e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\device\nuvoton\cm2003\include\system_CM2003.h:
+e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\device\nuvoton\cm2003\include\adc_reg.h:
+e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\device\nuvoton\cm2003\include\clk_reg.h:
+e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\device\nuvoton\cm2003\include\ecap_reg.h:
+e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\device\nuvoton\cm2003\include\fmc_reg.h:
+e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\device\nuvoton\cm2003\include\gpio_reg.h:
+e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\device\nuvoton\cm2003\include\i2c_reg.h:
+e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\device\nuvoton\cm2003\include\pwm_reg.h:
+e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\device\nuvoton\cm2003\include\sys_reg.h:
+e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\device\nuvoton\cm2003\include\timer_reg.h:
+e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\device\nuvoton\cm2003\include\uart_reg.h:
+e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\device\nuvoton\cm2003\include\uuart_reg.h:
+e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\device\nuvoton\cm2003\include\ui2c_reg.h:
+e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\device\nuvoton\cm2003\include\uspi_reg.h:
+e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\device\nuvoton\cm2003\include\wdt_reg.h:
+e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\device\nuvoton\cm2003\include\wwdt_reg.h:
+e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\stddriver\inc\adc.h:
+e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\stddriver\inc\clk.h:
+e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\stddriver\inc\ecap.h:
+e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\stddriver\inc\fmc.h:
+e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\stddriver\inc\gpio.h:
+e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\stddriver\inc\i2c.h:
+e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\stddriver\inc\sys.h:
+e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\stddriver\inc\timer.h:
+e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\stddriver\inc\uart.h:
+e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\stddriver\inc\pwm.h:
+e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\stddriver\inc\usci_i2c.h:
+e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\stddriver\inc\usci_spi.h:
+e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\stddriver\inc\usci_uart.h:
+e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\stddriver\inc\wdt.h:
+e:\working_zone\m23\5_cm2003\1_bsp_20241211_2\library\stddriver\inc\wwdt.h:
---
+++ SampleCode/StdDriver/FMC_MultiBoot/GCC/Release_Boot_0x4000/CMSIS/CMSIS/GCC/_syscalls.d
@@ -0,0 +1,2 @@
+CMSIS/CMSIS/GCC/_syscalls.o: \
+ E:/Working_zone/M23/5_CM2003/1_bsp_20241211_2/Library/Device/Nuvoton/CM2003/Source/GCC/_syscalls.c
